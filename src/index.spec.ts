77b46be74bb4e9315abb2bab4bfe88e121fb387f
Bono Fox <microfuchs@outlook.com>
2025-02-12 11:52:51 +0100

    ✨ First, simple, immutable builder
    - this builder uses a setter-method with string-keys
    - this builder is immutable, so each setter generates a new instance

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,51 @@
+import { TsBuilderImmutable } from "./index";
+
 describe("TsBuilder", () => {
-  it("should run", () => {
-    expect(() => {}).not.toThrow();
+  describe("Empty builder", () => {
+    interface EmptyInterface {}
+
+    it("should create a builder for an empty interface", () => {
+      const emptyInterfaceBuilder =
+        TsBuilderImmutable.makeBuilderFor<EmptyInterface>();
+
+      expect(emptyInterfaceBuilder).toBeDefined();
+    });
+
+    it("should create an empty interface", () => {
+      const emptyInterfaceBuilder =
+        TsBuilderImmutable.makeBuilderFor<EmptyInterface>();
+
+      expect(emptyInterfaceBuilder.build()).toEqual({});
+    });
+  });
+
+  describe("Simple interface", () => {
+    interface SimpleInterface {
+      textValue: string;
+      integerValue: number;
+      booleanValue: boolean;
+    }
+
+    it("should create a builder for a simple interface", () => {
+      const simpleInterfaceBuilder =
+        TsBuilderImmutable.makeBuilderFor<SimpleInterface>();
+
+      expect(simpleInterfaceBuilder).toBeDefined();
+    });
+
+    it("should create a simple interface", () => {
+      const simpleInterfaceBuilder =
+        TsBuilderImmutable.makeBuilderFor<SimpleInterface>();
+      const filledSimpleInterfaceBuilder = simpleInterfaceBuilder
+        .set("textValue", "Foo")
+        .set("integerValue", 42)
+        .set("booleanValue", true);
+
+      expect(filledSimpleInterfaceBuilder.build()).toEqual({
+        textValue: "Foo",
+        integerValue: 42,
+        booleanValue: true,
+      });
+    });
   });
 });
